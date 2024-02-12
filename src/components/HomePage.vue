138d06a0feefcd43caff54d2383db8337108465d
firemasterleo <firemasterleo@outlook.com>
2024-02-12 16:50:30 +0100

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -570,7 +570,7 @@ onMounted(() => {
   const tl2 = gsap.timeline({ defaults: { duration: 0.4, ease: 'power.in()' } });
   const tl3 = gsap.timeline({ defaults: { duration: 2, ease: 'power.in()' } });
   const tl4 = gsap.timeline({ defaults: { duration: 5, ease: 'power.in()' } });
-  const tl5 = gsap.timeline({ defaults: { duration: 0.8, ease: 'power.in()' } });
+  const tl5 = gsap.timeline({ defaults: { duration: 0.5, ease: 'power.in()' } });
 
   tl.from(first.value, { y: 80, delay: 0.06 });
   tl.from(second.value, { y: 80, delay: 0.09 }, '<'); // Add the second animation to start immediately after the first one
@@ -611,8 +611,8 @@ onMounted(() => {
   tl4.to(wind4.value, { x: 450, delay: 0 }, '<'); // Add the third animation to start immediately after the second one
 
 
-  tl5.from(about.value, { y: 80, delay: 0.06 });
-  tl5.from(shapestation.value, { y: 80, delay: 0.09 }, '<'); // Add the second animation to start immediately after the first one
+  tl5.from(about.value, { y: 80, delay: 0 }, '<');
+  tl5.from(shapestation.value, { y: 80, delay: 0.1 },'<'); // Add the second animation to start immediately after the first one
 
   ScrollTrigger.create({
     trigger: first.value,
