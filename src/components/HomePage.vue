379d22a3ed2e6256774c166fdc720e7b8e1d0811
firemasterleo <firemasterleo@outlook.com>
2024-02-20 08:48:55 +0100

    commit

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,6 +8,7 @@
         <img src="../assets/shapestationimage6.jpg" alt="">
     </div>
     <div class="herosection">
+
         
         <div class="heroheading">
    
@@ -20,7 +21,7 @@
                 <h1 ref="fourth">Brand</h1>
                 <h1 ref="fifth">Development</h1>
             </div>
-            <p>To suit and serve your brand, we build Modern, smoothe and intuitive webpages that are easy to navigate, guranteed to give your Users a great experience.</p>
+            <p>Our mission is simple: <br> To empower businesses with the necessary digital assets required to thrive in an ever-evolving digital landscape.</p>
             <div class="buttons">
                 <button class="button">About us</button>
                 <button class="button">Our Services</button>                
@@ -73,23 +74,7 @@
 
     </div>
     <div class="section2">
-        
-        <div class="about">
-            <!-- <div class="rocket">
-                <div class="svg" ref="svg">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24"><path d="M15.78 15.84S18.64 13 19.61 12c3.07-3 1.54-9.18 1.54-9.18S15 1.29 12 4.36C9.66 6.64 8.14 8.22 8.14 8.22S4.3 7.42 2 9.72L14.25 22c2.3-2.33 1.53-6.16 1.53-6.16zm-1.5-9a2 2 0 0 1 2.83 0 2 2 0 1 1-2.83 0zM3 21a7.81 7.81 0 0 0 5-2l-3-3c-2 1-2 5-2 5z"/></svg>
-                </div>
-
-                <div class="wind">
-
-                    <div class="wind1" ref="wind1"></div>
-                    <div class="wind2" ref="wind2"></div>
-                    <div class="wind3" ref="wind3"></div>
-                    <div class="wind4" ref="wind4"></div>
-
-                </div>
-
-            </div> -->
+        <div class="aboutus">
             <div class="aboutshapestation">
                 <div class="about" ref="about">
                     <h2>About</h2>
@@ -104,99 +89,21 @@
             <p>  We're a team of passionate and creative individuals and our mission is simple: to empower businesses with the tools they need to thrive in an ever-evolving digital landscape.
                 <br><br> We're here to enhance your online presence and help you stand out in a crowded digital marketplace
             </p>
-                  <!-- <div class="portfolio" :class="{ 'is-portfolioexpanded': is_portfolioexpanded }" @click="TogglePortfolio">
-                    <h2>Portfolio</h2>
-                    <svg :class="{ 'is-portfolioexpanded': is_portfolioexpanded }" xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
-                             
-                </div> -->
-        </div>
-        <div class="Our services">
 
-            <div class="svg"></div>
-            <h2>Contact Us</h2>
+
+        </div>
+
+        <div class="oursolutions">
+            <h2>Our Solutions</h2>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                  Placeat commodi ipsa modi amet exercitationem cum
                   voluptate, sint enim esse culpa, mollitia vitae in dolore nobis facilis provident, aperiam rem explicabo?</p>
-                  <div class="svg"></div>
         </div>
-        <div class="review">
 
-            <div class="svg"></div>
-            <h2>Our Workflow</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                 Placeat commodi ipsa modi amet exercitationem cum
-                  voluptate, sint enim esse culpa, mollitia vitae in dolore nobis facilis provident, aperiam rem explicabo?</p>
-                  <div class="svg"></div>
-        </div>
-        <!-- <div class="tools">
-            <div class="circle"></div>
-            <div class="svg">
-                <div>
-                    <svg class="css3" ref="css3" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path d="M4.192 3.143h15.615l-1.42 16.034-6.404 1.812-6.369-1.813L4.192 3.143zM16.9 6.424l-9.8-.002.158 1.949 7.529.002-.189 2.02H9.66l.179 1.913h4.597l-.272 2.62-2.164.598-2.197-.603-.141-1.569h-1.94l.216 2.867L12 17.484l3.995-1.137.905-9.923z"></path></svg>
-
-                </div>
-                <div>
-                    <svg class="java" ref="java" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path d="M9.37 17.51c-3.09.86 1.87 2.64 5.8 1a7.18 7.18 0 0 1-1.1-.54 11.59 11.59 0 0 1-4.16.18c-1.31-.15-.54-.64-.54-.64zm5.33-1.68a16.27 16.27 0 0 1-5.36.26c-1.31-.14-.45-.77-.45-.77-3.4 1.12 1.89 2.4 6.64 1a2.25 2.25 0 0 1-.83-.49zm1.82-9.73s-6.87 1.71-3.59 5.49a1.47 1.47 0 0 1-.25 2.12s2.46-1.27 1.33-2.86-1.86-2.22 2.51-4.75z"></path><path d="M18.13 19.14s.56.46-.63.82c-2.27.69-9.43.9-11.42 0-.72-.31.62-.74 1-.83a2.54 2.54 0 0 1 .69-.08c-.8-.56-5.15 1.1-2.21 1.58 8.05 1.37 14.64-.56 12.57-1.49zm-2.76-4.88a7 7 0 0 1 .92-.49s-1.51.27-3 .4a28.11 28.11 0 0 1-4.82.05C6.09 13.91 9.74 13 9.74 13a6.81 6.81 0 0 0-3.16.75c-2.06 1.03 5.09 1.48 8.79.51zm.91 2.44a.39.39 0 0 1-.08.09c5-1.32 3.18-4.65.78-3.81a.71.71 0 0 0-.33.25s.14-.05.43-.12c1.22-.25 2.92 1.63-.8 3.59zM12 13c-.74-1.67-3.26-3.14 0-5.71C16.09 4.08 14 2 14 2c.84 3.31-3 4.31-4.34 6.37-.93 1.41.47 2.92 2.34 4.63z"></path><path d="M16.41 21.32a29.75 29.75 0 0 1-9.14.14s.46.38 2.84.53c3.61.23 9.15-.12 9.29-1.83 0 0-.26.64-2.99 1.16z"></path></svg>
-
-                </div>
-                <div>
-                    <svg class="netlify" ref="netlify" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path d="M17.13 9.68 22 11.8v-.11c-.07-.24-.28-.45-.7-.87l-2-2-2.07.87zM12.2 7.24a1.31 1.31 0 0 1 .16.47l3 1.29a1.11 1.11 0 0 1 .29-.14l.48-3-2-2-1.95 3.3s0 .05.02.08zM10.61 7a1.1 1.1 0 0 1 1-.2h.05l2.16-3.34-.66-.66c-.42-.42-.63-.63-.87-.7a1 1 0 0 0-.63 0c-.24.07-.45.28-.87.7l-3 3c.31.14 2.5 1.06 2.78 1.19zm-3.12 5.29h.27a1.45 1.45 0 0 1 .29 0l2.38-3.68v-.06a1 1 0 0 1-.27-.95L9 7l-1.7-.73L5 8.61l2.51 3.66zm7.62-2.44v-.08a.59.59 0 0 1 0-.14l-2.86-1.27h-.05a1.05 1.05 0 0 1-.88.47h-.17s-.05 0-.06.05L8.73 12.5v.05l6.33-2.7s.05.02.05 0zm1.36-.91a.83.83 0 0 1 .26.19l2-.84-1.89-1.87-.4 2.47zm-2.29 5.54A.91.91 0 0 1 15 14v-.05l.54-3.37-.09-.08L9 13.15a2.76 2.76 0 0 1 .08.27zM2 11.95zm12.14 3.18L8.92 14v.06L13.47 21l.42-.43c.15-.92.71-4.33.78-4.72a.9.9 0 0 1-.53-.72zm1.65.32a.88.88 0 0 1-.36.27l-.64 3.94L18.43 16zm1.07-5.13a.07.07 0 0 0-.06 0 .92.92 0 0 1-.61.34v.06l-.55 3.4a.88.88 0 0 1 .34.62h.06l2.95.61 2.28-2.28a4.22 4.22 0 0 0 .65-.74zM8.3 14.59a1.27 1.27 0 0 1-.86 0l-1.22 1.9 4.69 4.68a2.68 2.68 0 0 0 .87.71 1 1 0 0 0 .63 0 1.86 1.86 0 0 0 .7-.54l-4.69-6.8zm4.71 6.82zm-6.62-7.82s.06 0 .06-.05 0 0-.06 0l-4.14-.86a7.07 7.07 0 0 0 .51.54l1.36 1.36 2.27-1zm.28-.66a1.05 1.05 0 0 1 .2-.27v-.06C6.1 11.4 5.3 10.24 4.5 9.07l-1.75 1.75c-.42.42-.63.63-.7.87A.75.75 0 0 0 2 12l4.67 1zm.17 1.33a.61.61 0 0 1-.07-.1L4.59 15l1.08 1.1 1.17-1.82s.01-.02 0-.02z"></path></svg>
-
-                </div>
-                <div>
-                    <svg class="vuejs" ref="vuejs" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path d="m12 12.765 5.592-9.437h-3.276L12 7.33v.002L9.688 3.328h-3.28z"></path><path d="M18.461 3.332 12 14.235 5.539 3.332H1.992L12 20.672l10.008-17.34z"></path></svg>
-
-                </div>
-                <div>
-                    <svg class="html5" ref="html5" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path d="M4.136 3.012h15.729l-1.431 16.15-6.451 1.826-6.414-1.826-1.433-16.15zm5.266 7.302-.173-2.035 7.533.002.173-1.963-9.87-.002.522 5.998h6.835l-.243 2.566-2.179.602-2.214-.605-.141-1.58H7.691l.247 3.123L12 17.506l4.028-1.08.558-6.111H9.402v-.001z"></path></svg>
-                </div>
-                <div>
-                    <svg class="coding" ref="coding" xmlns='http://www.w3.org/2000/svg'  viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path d="m7.375 16.781 1.25-1.562L4.601 12l4.024-3.219-1.25-1.562-5 4a1 1 0 0 0 0 1.562l5 4zm9.25-9.562-1.25 1.562L19.399 12l-4.024 3.219 1.25 1.562 5-4a1 1 0 0 0 0-1.562l-5-4zm-1.649-4.003-4 18-1.953-.434 4-18z"></path></svg>
-
-
-                </div>
-                <div>
-                    <svg class="git" ref="git" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='#000000' width='34' height='34'><path fill-rule="evenodd" clip-rule="evenodd" d="M12.026 2c-5.509 0-9.974 4.465-9.974 9.974 0 4.406 2.857 8.145 6.821 9.465.499.09.679-.217.679-.481 0-.237-.008-.865-.011-1.696-2.775.602-3.361-1.338-3.361-1.338-.452-1.152-1.107-1.459-1.107-1.459-.905-.619.069-.605.069-.605 1.002.07 1.527 1.028 1.527 1.028.89 1.524 2.336 1.084 2.902.829.091-.645.351-1.085.635-1.334-2.214-.251-4.542-1.107-4.542-4.93 0-1.087.389-1.979 1.024-2.675-.101-.253-.446-1.268.099-2.64 0 0 .837-.269 2.742 1.021a9.582 9.582 0 0 1 2.496-.336 9.554 9.554 0 0 1 2.496.336c1.906-1.291 2.742-1.021 2.742-1.021.545 1.372.203 2.387.099 2.64.64.696 1.024 1.587 1.024 2.675 0 3.833-2.33 4.675-4.552 4.922.355.308.675.916.675 1.846 0 1.334-.012 2.41-.012 2.737 0 .267.178.577.687.479C19.146 20.115 22 16.379 22 11.974 22 6.465 17.535 2 12.026 2z"></path></svg>
-
-                </div>
-                
-            </div>
-
-        </div> -->
         </div>
         <!-- <div class="section3">
         </div> -->
-        <div class="section4">
-            <div class="veil"></div>
-
-            <div class="leftsection">
-
-                    <h1 class="top">VIE MODERNE</h1>
-                    <h1 class="bottom">VIE MODERNE</h1>
-                    <p>your space, your personality</p>
-                    <button>Order a Project</button>
-                </div>
-                <div class="contact">
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                </div>
-
-            <div class="rightsection">
-                <div class="image">
-                    <img src="../assets/interiorimage1.jpeg" alt="">
-                </div>
-                <div class="smallbox">
-                    <h2></h2>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <p></p>
-                    <button>DISCOVER MORE</button>
-                </div>
-
-            </div>
-        </div>
+        
 
 
 </div>
@@ -238,7 +145,6 @@
     .herosection {
         // border: solid;
         background-color: rgba(0, 0, 0, 0);
-        color: white;
         height: 45rem;
         padding-left: 1.5rem;
         padding-right: 1.5rem;
@@ -304,11 +210,16 @@
   }
 
         .herosliders {
+            margin-inline: -1.5rem;
             display: flex;
             flex-direction: column;
             gap: 1rem;
             // border: solid;
             margin-top: 4rem;
+            // box-shadow: inset 2rem 0px 0px 0px rgba(255, 255, 255, 0.993),
+            //             inset  -2rem 0px 0px 0px rgba(255, 255, 255, 0.333);
+            // background-color: white;
+
 
             @keyframes scrollAnimation {
                 0% {
@@ -322,7 +233,6 @@
                 display: flex;
                 overflow: hidden;
                 gap: 1rem;
-                box-shadow: -2px 0 50px rgba(0, 0, 0, 0.699); /* Apply a box shadow to the left side */
 
                 .innerslider {
                     display: flex;
@@ -450,74 +360,13 @@
     }
     .section2 {
         padding-inline: 1.2rem;
-        background-color: rgb(62, 69, 56);
-        // height: fit-content;
-
-
-        .review1 {
-            display: flex;
-            flex-direction: column;
-            gap: 1rem;
-            height: 30rem;
-            padding-top: 2rem;
-            position: relative;
-            z-index: 0;
-            // border: solid;
-            // margin: 1rem 0;
-
-            // .rocket {
-            //     // border: solid;
-            //     height: 15rem;
-            //     width: 7rem;
-            //     position: absolute;
-            //     z-index: -2;
-
-            //     right: 1rem;
-            //     top: 0rem;
-    
-            //     .svg {
-            //         position: absolute;
-            //         z-index: -2;
-            //         left: 2.4rem;
-            //         top: 10rem;
-            //         rotate: -45deg;
-    
-            //     }
-
-            //     .wind {
-            //         display: flex;
-            //         flex-direction: column;
-            //         margin-top: 2rem;
-            //         gap: 0.3rem;
-            //         // margin-left: 1.1rem;
-            //         // margin-top: 5rem;
-            //         overflow: hidden;
-            //         rotate: -90deg;
-            //         // border: solid;
-            //         width: 18rem;
-            //         position: relative;
-            //         top: 1rem;
-            //         right: 5.5rem;
-            //         div {
-            //             border-bottom: solid 1.5px black;
-            //             width: 2rem;
-            //             margin-right: 4rem;
-            //             // position: absolute;
-            //         }
-            //         .wind1 {
-            //             margin-left: -2rem;
-            //         }
-            //         .wind2 {
-            //             margin-left: -4rem;
-            //         }
-            //         .wind3 {
-            //             margin-left: -4rem;
-            //         }
-            //         .wind4 {
-            //             margin-left: -2rem;
-            //         }
-            //     }
-            // }   
+        background-color: rgba(0, 0, 0, 0.342);
+        height: 40rem;
+        display: flex;
+        flex-direction: column;
+        gap: 4rem;
+        padding-top: 2rem;
+        .aboutus {
             .aboutshapestation {
                 display: flex;
                 gap: 0.5rem;
@@ -535,100 +384,13 @@
                 color: rgb(202, 196, 196);
 
             }
-        .portfolio {
-            border-top:  solid rgb(169, 163, 163);
-            border-bottom:  solid 1px rgb(169, 163, 163);
-            height: 3.5rem;
-            padding-top: 0.5rem;
-            display: flex;
-            justify-content: space-between;
-            letter-spacing: 0.1rem;
-            margin-top: -1rem;
-            margin-bottom: 4rem;
-            transition: 0.3s ease-in;
-            background-color: rgba(25, 24, 24, 0.804);
-            // border: solid;
-
-            h2 {
-                font-size: 24px;
-
-
-            }
-
-
-            &.is-portfolioexpanded {
-                height: 30rem;
-                transition: 0.3s ease-in;
-            }
-
-
-        } 
-        svg {
-            margin-right: 1rem;
-            transition: 0.3s ease-in;
-
-
-            &.is-portfolioexpanded {
-                transform: rotateZ(135deg);
-                transition: 0.3s ease-in;
-
-            }
-        }
-
 
         }
-        .review {
-            display: flex;
-            flex-direction: column;
-            gap: 1.5rem;
-            padding-top: 2rem;
-        
 
-            h2 {
-                font-size: 26px;
+            .oursolutions {
+                color: rgb(202, 196, 196);
 
             }
-            p {
-                font-size: 18px;
-                width: 23rem;
-
-            }
-
-        }
-        .tools {
-            border: solid rgba(126, 155, 120, 0.379);
-            overflow: hidden;
-
-            // width: 11rem;
-            .circle {
-                height: 17rem;
-                width: 80%;
-                border: 50%;
-                // border: solid rgba(126, 155, 120, 0.941);
-                position: absolute;
-                padding: 1rem;
-
-            }
-            .svg {
-                width: 19rem;
-                display: flex;
-                justify-content: space-between;
-                // border: solid;
-                overflow: hidden;
-                margin-left: 1rem;
-
-                div {
-                    svg {
-                         
-                        
-                    }
-                    
-
-                }
-
-
-            }
-        }
     }
     .section3 {
         width: 100vw; /* Adjust width as needed */
